19c8ce102ee897e9ab0ad389a17fd200b5adcfca
GrassiOS <148152704+GrassiOS@users.noreply.github.com>
2025-03-25 19:30:35 -0700

    Opps Controller

---
+++ Business/contracts/I_opps_service.cs
@@ -0,0 +1,16 @@
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Contracts
+{
+    public interface IOppsService
+    {
+        int Add(OppsAdd opps);
+        Opps? Get(int id);
+        bool Delete(int id);
+    }
+}
---
+++ Business/implementations/opps_service.cs
@@ -0,0 +1,40 @@
+using Business.Contracts;
+using Data.contracts;
+using Data.Contracts;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Implementations
+{
+    public class OppsService : IOppsService
+    {
+        private readonly IOppsRepository _service;
+
+        public OppsService(IOppsRepository service)
+        {
+            _service = service;
+        }
+
+        public int Add(OppsAdd opps)
+        {
+            if(opps == null) { return 0; }
+            return _service.Add(opps);
+        }
+
+        public bool Delete(int id)
+        {
+            if(id < 1) { return false; }
+            return _service.Delete(id);
+        }
+
+        public Opps? Get(int id)
+        {
+            if(id < 1) { return null; }
+            return _service.Get(id);
+        }
+    }
+}
