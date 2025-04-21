65d778aac2921d4c2d5c8fb85ed000cec37e2376
GrassiOS <148152704+GrassiOS@users.noreply.github.com>
2025-04-20 17:03:28 -0700

    ach2Pat FIx

--- Domain/patient/patient.cs
+++ Domain/patient/patient.cs
@@ -39,7 +39,7 @@ namespace Domain
         /// </summary>
         public Diary diary { get; set; } = new Diary();
 
-        public AchievementCollection? achievementCollection { get; set; }
+        public AchievementCollection? achievementCollection { get; set; } = new AchievementCollection();
 
         /// <summary>
         /// A list of carts associated with the patient (e.g., shopping carts, action items).
