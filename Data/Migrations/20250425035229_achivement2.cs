7ce24a8cac60c2c79bbb5e101b3d50eb8ae2db7f
Armando Alvarado <76576669+ArmandoAl@users.noreply.github.com>
2025-04-24 21:10:55 -0700

    migrtion success

---
+++ Data/Migrations/20250425035229_achivement2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class achivement2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
